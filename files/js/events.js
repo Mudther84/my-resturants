3a6d1efcc73542fb9c6b5a5a3b02b0a93e61798d
Mudther <moudaseryassin@gmail.com>
2024-12-22 23:31:22 +0200

    first commit

---
+++ files/js/events.js
@@ -0,0 +1,27 @@
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let txt = document.getElementById('subject');
+let oasis = document.getElementById('name');
+let txtarea = document.getElementById('Message');
+window.onload = function() {
+    //email
+   email.style.borderColor='orange';
+   email.style.borderWidth='1px';
+   email.style.borderStyle='solide';
+   //phone element
+   phone.style.borderColor='orange';
+   phone.style.borderWidth='1px';
+   phone.style.borderStyle='solide';
+   //txt element
+   txt.style.borderColor='orange';
+   txt.style.borderWidth='1px';
+   txt.style.borderStyle='solide';
+   //texet area element
+   txtarea.style.borderColor='orange';
+   txtarea.style.borderWidth='1px';
+   txtarea.style.borderStyle='solide';
+    //name element
+    txt1.style.borderColor='orange';
+    txt1.style.borderWidth='1px';
+    txt1.style.borderStyle='solide';
+}
